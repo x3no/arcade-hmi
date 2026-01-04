780f099235eff081882323796483c423c066269d
Alberto <alberto.rodriguez@wallbox.com>
2026-01-04 16:27:30 +0100

    draft

--- start_x11.sh
+++ start_x11.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Start application with X11
 
-# Start X server and run the app
-xinit /usr/bin/python3 /home/dietpi/arcade-hmi/src/main.py -- :0 vt1 -nocursor
+# Set correct display resolution
+export DISPLAY=:0
+
+# Start X server with 1920x1080 resolution and run the app
+xinit /usr/bin/python3 /home/dietpi/arcade-hmi/src/main.py -- :0 vt1 -nocursor -screen 0 1920x1080x24
